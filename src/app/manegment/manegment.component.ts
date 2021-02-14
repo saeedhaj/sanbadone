764ac9f65325c58b3367b698f22d3b97dcf54581
saeedhaj <saeed.hajiaghzadeh@gmail.com>
2021-02-14 10:34:09 +0330

    Install Angular Material and Flex Layout  and code in component manegment

--- src/app/manegment/manegment.component.ts
+++ src/app/manegment/manegment.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManegmentComponent implements OnInit {
 
+  showFiller = false;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/manegment/manegment.module.ts
+++ src/app/manegment/manegment.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule} from "@angular/forms";
 import { ManegmentRoutingModule } from './manegment-routing.module';
 import { ManegmentComponent } from './manegment.component';
 import { CityModule } from "./city/city.module";
 import { StateModule } from "./state/state.module";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+const MATERIAL_MODULE=[
+  MatSidenavModule,
+  MatButtonModule,
+  MatMenuModule
+];
 
 @NgModule({
   declarations: [ManegmentComponent],
   imports: [
     CommonModule,
     ManegmentRoutingModule,
+    ...MATERIAL_MODULE,
+    FormsModule,
     CityModule,
-    StateModule
+    StateModule,
+    FlexLayoutModule
   ],
   exports: [ManegmentComponent]
 })
