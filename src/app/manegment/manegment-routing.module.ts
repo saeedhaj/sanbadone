196daea278d12687a351d4c71fdc19b1d31a0345
saeedhaj <saeed.hajiaghzadeh@gmail.com>
2021-02-14 09:27:09 +0330

    add  address city to component manegment and put router-outlet

--- src/app/manegment/manegment-routing.module.ts
+++ src/app/manegment/manegment-routing.module.ts
@@ -5,9 +5,37 @@ import { ManegmentComponent } from "./manegment.component";
 const routes: Routes = [
   {
     path:'',
-    component:ManegmentComponent
+    component:ManegmentComponent,
+    children:[
+      {
+        path: '',
+        redirectTo:'city',
+      },
+      {
+        path: 'city',
+        loadChildren:()=>import('./city/city.module').then((m)=>m.CityModule),
+      },
+      // {
+      //   path: 'roles',
+      //   loadChildren:()=>import('./roles/roles.module').then((m)=>m.RolesModule),
+      // },
+
+      // {
+      //   path: 'sponsors',
+      //   loadChildren:()=>import('./sponsors/sponsors.module').then((m)=>m.SponsorsModule),
+      // },
+      // {
+      //   path: 'events',
+      //   loadChildren:()=>import('./events/events.module').then((m)=>m.EventsModule),
+      // },
+      // {
+      //   path: 'calendars',
+      //   loadChildren:()=>import('./calendars/calendars.module').then((m)=>m.CalendarsModule),
+      // },
+    ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/manegment/manegment.module.ts
+++ src/app/manegment/manegment.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ManegmentRoutingModule } from './manegment-routing.module';
 import { ManegmentComponent } from './manegment.component';
+import { CityModule } from "./city/city.module";
 
 
 @NgModule({
   declarations: [ManegmentComponent],
   imports: [
     CommonModule,
-    ManegmentRoutingModule
+    ManegmentRoutingModule,
+    CityModule
   ],
   exports: [ManegmentComponent]
 })
